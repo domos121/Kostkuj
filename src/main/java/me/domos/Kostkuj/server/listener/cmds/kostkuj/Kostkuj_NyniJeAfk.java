80aa46a4bfadf20953ece6ea35ed0ec7d951429f
domos121 <dominik.lau46gmail.com>
2019-03-10 13:51:01 +0100

    Fix /k nynijeafk.

--- src/main/java/me/domos/Kostkuj/server/listener/cmds/kostkuj/Kostkuj_NyniJeAfk.java
+++ src/main/java/me/domos/Kostkuj/server/listener/cmds/kostkuj/Kostkuj_NyniJeAfk.java
@@ -28,7 +28,7 @@ public class Kostkuj_NyniJeAfk {
             if (!op.isOnline()) {
                 this.ss.info(sr, "Hrac neni online.");
             } else {
-                Player p = (Player) sr;
+                Player p = (Player) op;
                 Location l = p.getLocation();
                 String json = this.cjb.vetaClickHoverText("§6Hráč §r", "", p.getDisplayName(), "", p.getDisplayName() + "§7:\nLokace: §cx:" + l.getBlockX() + ", y:" + l.getBlockY() + ", z:" + l.getBlockZ() + "\n§7Svet: §c" + l.getWorld().getName().trim(), "suggest_command", "/tppos " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ() + " " + l.getWorld().getName().trim(), "§6 je afk více než 30 min.", "");
                 this.jbc.jsonBcKostkuj(json, ECmd.KOSTKUJ_NYNIJEAFK.getPerm());
